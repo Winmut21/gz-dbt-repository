fa9e1e4037b6bac90fcafee055b154ee6f99791f
mutaiwinnie@gmail.com <mutaiwinnie@gmail.com>
2024-08-06 21:01:46 +0000

    calculated purchase cost and margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -3,7 +3,10 @@ select
         orders_id,
         products_id,
         revenue,
-        quantity
+        quantity,
+        purchase_price,
+       ROUND( quantity * CAST(purchase_price AS FLOAT64 ),2) AS purchase_cost,
+       ROUND( revenue - CAST(purchase_price AS FLOAT64 ),2) AS margin
 from {{ ref('stg_raw__sales') }} s
 join {{ ref('stg_raw__product') }} p
 using (products_id)
